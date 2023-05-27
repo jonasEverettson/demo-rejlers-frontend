8c5ab9d129852d6ca942840b4e026102e2717837
jonasEverettson <jonas.everettson@hotmail.com>
2023-05-27 10:33:53 +0200

    added csv download

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,9 +1,29 @@
+import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
 const Admin = () => {
   const navigate = useNavigate();
+
+
+  const downloadCSV = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/v1/downloadBookings",
+        { responseType: "blob" }
+      );
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "ListOrders.csv");
+      link.click();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 
 
@@ -25,6 +45,15 @@ return (
     >
       Till Anställda
     </a>
+  </button>
+  
+  <button>
+    <a 
+      onClick={downloadCSV}
+      className="rounded bg-slate-600 text-white px-2 py-2 m-1 font-semibold hover:bg-slate-400"
+      >
+        Ladda hem CSV
+      </a>
   </button>
   </div>
   
